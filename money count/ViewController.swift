eb0ee1a3f9b3d825f24298e7c57cdb98176c7b0f
Aaron Kuan <10851035@st.fhjh.tp.edu.tw>
2020-02-03 15:43:41 +0800

    Initial Commit

---
+++ money count/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  money count
+//
+//  Created by Aaron Kuan on 2020/2/3.
+//  Copyright © 2020 Aaron Kuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
